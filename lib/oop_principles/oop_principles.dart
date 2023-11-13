49544c7e61947dfdbc2331b599aa3cc08d214fc4
ahmedabdelrahmanalghwalbi <https://github.com/ahmedabdelrahmanalghwalbi>
2023-11-13 13:24:50 +0200

    #basics

---
+++ lib/oop_principles/oop_principles.dart
@@ -0,0 +1,43 @@
+/*
+1- Data Hiding :-
+---------------
+In OO terminology, data is referred to as attributes, and behaviors are referred to as methods.
+Restricting access to certain attributes and/or methods is called data hiding.
+By combining the attributes and methods in the same entity, which in OO parlance is
+called encapsulation.
+
+2- Getters and Setters :-
+----------------------
+The concept of getters and setters supports the concept of data hiding. Because other objects
+should not directly manipulate data within another object, the getters and setters provide
+controlled access to an object’s data. Getters and setters are sometimes called
+accessor methods and mutator methods, respectively.
+
+3- Modeling Tools :-
+------------------
+Visual modeling tools provide a mechanism to create and manipulate class diagrams using
+the Unified Modeling Language (UML).
+
+4- What Exactly Is a Class?
+--------------------------
+In short, a class is a blueprint for an object.When you instantiate an object, you use a class
+as the basis for how the object is built.
+Note :- Classes Are Object Templates.
+
+5- Access Designations
+-----------------------
+When a data type or method is defined as public, other objects can directly access it.
+When a data type or method is defined as private, only that specific object can access it.
+Another access modifier, protected, allows access by related objects,
+
+
+6- Methods:-
+-----------
+methods implement the required behavior of a class.
+
+7- Messages
+-----------
+Messages are the communication mechanism between objects.
+ 
+ 
+ */
